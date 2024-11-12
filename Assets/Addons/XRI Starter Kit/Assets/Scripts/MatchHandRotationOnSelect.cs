6cb2bc1cfb2e9713c39760e6719fd06e15038372
Artyom <159782150+PISHNGTU@users.noreply.github.com>
2024-11-12 03:39:43 +0300

    upload project

---
+++ Assets/Addons/XRI Starter Kit/Assets/Scripts/MatchHandRotationOnSelect.cs
@@ -0,0 +1,31 @@
+﻿// Author MikeNspired. 
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace MikeNspired.UnityXRHandPoser
+{
+    public class MatchHandRotationOnSelect : MonoBehaviour
+    {
+        public XRBaseInteractable interactable;
+        public Transform HandAttachTransformParent;
+
+        private void OnValidate()
+        {
+            if (!interactable) interactable = GetComponent<XRBaseInteractable>();
+        }
+
+        private void Start()
+        {
+            OnValidate();
+            interactable.selectEntered.AddListener(x => SetPosition(x.interactorObject.transform.GetComponentInParent<HandReference>()?.Hand));
+        }
+
+        private void SetPosition(HandAnimator handAnimator)
+        {
+            var handDirection = handAnimator.transform.forward;
+            HandAttachTransformParent.transform.forward = Vector3.ProjectOnPlane(handDirection, transform.up);
+            HandAttachTransformParent.transform.Rotate(Vector3.forward, -90f);
+        }
+    }
+}
